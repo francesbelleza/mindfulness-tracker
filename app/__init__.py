b8b89f48c513e5d2b5b0ad3d9dc20858b72bd924
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-04-07 21:01:58 -0700

    fixed connection b/w init & mindfulness_tracker_app

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,11 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     db.init_app(app)
 
+    # imports my routes
+    with app.app_context():
+        from app import mindfulness_tracker_app
+        mindfulness_tracker_app.initial_routes(app)
+
     return app
-
-#imports my routes
-from app import mindfulness_tracker_app
-
--- app/mindfulness_tracker_app.py
+++ app/mindfulness_tracker_app.py
@@ -1,14 +1,11 @@
-# Created by Frances Belleza -  Mindfulness Tracker
-#
-# File: app/ mindfulness_tracker_app.py
+# By Frances Belleza
 # Function: This file is like main()
 #              it defines my routes & logic
 
-from app import app
 from flask import render_template
 
-@app.route('/')
-@app.route('/mindfulness-tracker-home')
-def home():
-    user_logged_in = False #need to change to True to test dashboard button
-    return render_template('index.html', user_logged_in=user_logged_in)
+def initial_routes(app):
+    @app.route('/mindfulness-tracker-home')
+    def home():
+        user_logged_in = False #need to change to True to test dashboard button
+        return render_template('index.html', user_logged_in=user_logged_in)
