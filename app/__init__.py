f0e78d835384a7d028a021accdaee11c13469495
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-05-04 17:06:16 -0700

    continued sprint 1, set up front end

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ migrate = Migrate()
 login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
 
     db.init_app(app)
